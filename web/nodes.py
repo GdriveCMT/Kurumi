95c7b372805b44ad089a8565d9d79e239ce7d6f7
GdriveCMT <106360345+GdriveCMT@users.noreply.github.com>
2023-11-21 09:47:03 +0700

    Fix anytree version conflict

--- web/nodes.py
+++ web/nodes.py
@@ -1,7 +1,6 @@
 from anytree import NodeMixin
-from os import environ
 from re import findall as re_findall
-
+from os import environ
 
 DOWNLOAD_DIR = environ.get('DOWNLOAD_DIR', '')
 if len(DOWNLOAD_DIR) == 0:
@@ -20,7 +19,7 @@ class TorNode(NodeMixin):
         if parent is not None:
             self.parent = parent
         if size is not None:
-            self.size = size
+            self.fsize = size
         if priority is not None:
             self.priority = priority
         if file_id is not None:
@@ -32,11 +31,9 @@ class TorNode(NodeMixin):
 def qb_get_folders(path):
     return path.split("/")
 
-
 def get_folders(path):
     fs = re_findall(f'{DOWNLOAD_DIR}[0-9]+/(.+)', path)[0]
     return fs.split('/')
-
 
 def make_tree(res, aria2=False):
     parent = TorNode("Torrent")
@@ -77,6 +74,12 @@ def make_tree(res, aria2=False):
                         file_id=i['index'], progress=round((int(i['completedLength'])/int(i['length']))*100, 5))
     return create_list(parent, ["", 0])
 
+"""
+def print_tree(parent):
+    for pre, _, node in RenderTree(parent):
+        treestr = u"%s%s" % (pre, node.name)
+        print(treestr.ljust(8), node.is_folder, node.is_file)
+"""
 
 def create_list(par, msg):
     if par.name != ".unwanted":
@@ -92,9 +95,9 @@ def create_list(par, msg):
         else:
             msg[0] += '<li>'
             if i.priority == 0:
-                msg[0] += f'<input type="checkbox" name="filenode_{i.file_id}" data-size="{i.size}"> <label data-size="{i.size}" for="filenode_{i.file_id}">{i.name}</label> / {i.progress}%'
+                msg[0] += f'<input type="checkbox" name="filenode_{i.file_id}" data-size="{i.fsize}"> <label data-size="{i.fsize}" for="filenode_{i.file_id}">{i.name}</label> / {i.progress}%'
             else:
-                msg[0] += f'<input type="checkbox" checked name="filenode_{i.file_id}" data-size="{i.size}"> <label data-size="{i.size}" for="filenode_{i.file_id}">{i.name}</label> / {i.progress}%'
+                msg[0] += f'<input type="checkbox" checked name="filenode_{i.file_id}" data-size="{i.fsize}"> <label data-size="{i.fsize}" for="filenode_{i.file_id}">{i.name}</label> / {i.progress}%'
             msg[0] += f'<input type="hidden" value="off" name="filenode_{i.file_id}">'
             msg[0] += "</li>"
 
